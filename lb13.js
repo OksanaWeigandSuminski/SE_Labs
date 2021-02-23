5a5616198165e8484a22c8934f9a246f9d1d3ce9
Oksana Weigand-Suminski <oxanapiven@gmail.com>
2021-02-22 21:38:49 -0500

    cover for lab 13, main index

--- lb13.js
+++ lb13.js
@@ -10,7 +10,7 @@ var total = (snacks * 365) * (maxAge - age);
 document.getElementById("snacks").innerHTML = `You will need ${total} chocolate bars to last you until the ripe old age of ${maxAge}`
 
 var arr = [];
-arr.push(prompt("Enter your birthday: "));
+arr.push(prompt("Enter your birth year: "));
 arr.push(prompt("Enter future year: "));
 arr.push(arr[1] - arr[0]);
 document.getElementById("year").innerHTML = `You will be either ${arr[2]} or ${arr[2]+1} in the year of ${arr[1]}`;
