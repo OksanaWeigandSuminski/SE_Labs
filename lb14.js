bc758513148ef9a33539e74d8425a2e1538b6d08
Oksana Weigand-Suminski <oxanapiven@gmail.com>
2021-02-25 16:19:04 -0500

    working shopping cart

--- lb14.js
+++ lb14.js
@@ -1,13 +1,28 @@
 
 const veggies = [
-  {item: 'Potato', type: 'Russet', location: 'Idaho', price: 3.75, photo: 'images/potato.jpg'},
-  {item: 'Carrot', type: 'Danvers', location: 'Massachusetts', price: 2.50, photo: 'images/carrots.jpg'},
-  {item: 'Onion', type: 'Red Onion', location: 'California', price: 3.15, photo: 'images/onions.jpg'},
-  {item: 'Broccoli', type: 'Belstar', location: 'California', price: 5.50, photo: 'images/broccoli.jpg'},
-  {item: 'Tomato', type: 'Cocktail Tomatoes', location: 'Maine', price: 2.45, photo: 'images/tomato.jpg'},
-  {item:'Beetroot', type: 'Garden Beets', location: 'Oregon', price: 2.75, photo: 'images/beet.jpg'}
+  {item: 'Potato', type: 'Russet', location: 'Idaho', price: 3.75, qty: 2, photo: 'images/potato.jpg'},
+  {item: 'Carrot', type: 'Danvers', location: 'Massachusetts', price: 2.50, qty: 3, photo: 'images/carrots.jpg'},
+  {item: 'Onion', type: 'Red Onion', location: 'California', price: 3.15, qty: 4, photo: 'images/onions.jpg'},
+  {item: 'Broccoli', type: 'Belstar', location: 'California', price: 5.50, qty: 5, photo: 'images/broccoli.jpg'},
+  {item: 'Tomato', type: 'Cocktail Tomatoes', location: 'Maine', price: 2.45, qty: 6, photo: 'images/tomato.jpg'},
+  {item:'Beetroot', type: 'Garden Beets', location: 'Oregon', price: 2.75, qty: 7, photo: 'images/beet.jpg'}
 ];
-var quantity = [];
+var items = {
+  potato: 3.75,
+  carrot: 2.50,
+  onion: 3.15,
+  broccoli: 5.50,
+  tomato: 2.45,
+  beetroot: 2.75
+};
+var cart = {
+  potato: 0,
+  carrot: 0,
+  onion: 0,
+  broccoli: 0,
+  tomato: 0,
+  beetroot: 0
+};
 let html = ``;
 for (let i=0; i< veggies.length; i++){
   let veg = veggies[i];
@@ -16,23 +31,54 @@ for (let i=0; i< veggies.length; i++){
     <h2>${veg.item}</h2>
     <h3>${veg.type} | ${veg.location}</h3>
     <p>Price: ${veg.price}</p>
-    <form>
-  	<label for="qty">Quantity [kg]</label>
-  	<input type="text" name="qty" id="qty"/>
-    <p id="receipt"><input type="button" onclick="myFunction()" value="Check Out"/></p>
-    </form>
+    <button onclick="addItems()">Add Item</button><br><br>
 `;
-//document.getElementById("item").innerHTML = veg.item;
-function myFunction() {
-var qty = document.getElementById("qty").value;
-//document.getElementById("receipt").innerHTML = qty;
-var total = "Total price: " +  qty * veg.price;
-document.getElementById("total").innerHTML = total;
-//console.log('qty');
-quantity.push(qty);
-document.getElementById("array").innerHTML = "Total array: " + quantity;
-};
 }
-
+// add items to cart
+function addItems(){
+  var add = prompt("What item would you like to add? (Potato/Carrot/Onion/Broccoli/Tomato/Beetroot)");
+  var qty = prompt("Qty:");
+  if(add.toLowerCase() == "potato"){
+    cart.potato += Number(qty);
+    alert("You have added " + qty + " " + add.toLowerCase() + " to your cart.");
+  }
+  else if(add.toLowerCase() == "carrot"){
+    cart.carrot += Number(qty);
+    alert("You have added " + qty + " " + add.toLowerCase() + " to your cart.");
+  }
+  else if(add.toLowerCase() == "onion"){
+    cart.onion += Number(qty);
+    alert("You have added " + qty + " " + add.toLowerCase() + " to your cart.");
+  }
+  else if(add.toLowerCase() == "broccoli"){
+    cart.broccoli += Number(qty);
+    alert("You have added " + qty + " " + add.toLowerCase() + " to your cart.");
+  }
+  else if(add.toLowerCase() == "tomato"){
+    cart.tomato += Number(qty);
+    alert("You have added " + qty + " " + add.toLowerCase() + " to your cart.");
+  }
+  else if(add.toLowerCase() == "beetroot"){
+    cart.beetroot += Number(qty);
+    alert("You have added " + qty + " " + add.toLowerCase() + " to your cart.");
+  }
+  else{
+    alert("The item you entered does not exist in the available items. Please try again");
+  }
+}
+function myFunction() {
+  var text = "";
+  var total = 0;
+  for (property in cart){
+    item = `${property}`;
+    qty = `${cart[property]}`;
+    subtotal = (`${cart[property]}` * `${items[property]}`);
+    total += subtotal;
+    text += "Item: " + item + "  Qty: " + qty + "  Subtotal: $"  + subtotal;
+    text += '<br>'
+  }
+  text += "Total: $" + total;
+  document.getElementById("receipt").innerHTML += text;
+};
 console.log(html);
 document.querySelector('main').insertAdjacentHTML('beforeend', html);
