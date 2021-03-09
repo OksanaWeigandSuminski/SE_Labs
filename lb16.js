d2c552410f347aa9d664a0883c6df745dfda95a8
Oksana Weigand-Suminski <oxanapiven@gmail.com>
2021-03-08 21:40:22 -0500

    fixed js

--- lb16.js
+++ lb16.js
@@ -27,3 +27,22 @@ document.getElementById('imageReplace4').src = element;
 function changeImage5(element) {
 document.getElementById('imageReplace5').src = element;
 }
+
+//to get url of file type, in chrome and other webkit browsers
+var _URL = window.URL || window.webkitURL;
+
+$("#subbut").click(function () {
+    var file;
+    var image;
+    if ((file = $("#upload-image")[0].files[0])) {
+        image = new Image();
+        image.onload = function () {
+            $("#ninja").hide();
+            $("#oldImg").hide();
+            $("#image-wrapper").append(this);
+            $(this).attr("id", "image");
+        }
+        //static method creates a DOMString containing a URL representing the object given in the parameter
+        image.src = _URL.createObjectURL(file);
+    }
+});
--- practiceJQ.js
+++ practiceJQ.js
@@ -20,29 +20,3 @@ $(".kilo").click(function(){
   //   });
   // });
 });
-//to get url of file type, in chrome and other webkit browsers
-var _URL = window.URL || window.webkitURL;
-
-$("#subbut").click(function () {
-    var file;
-    var image;
-    if ((file = $("#upload-image")[0].files[0])) {
-        image = new Image();
-        image.onload = function () {
-            $("#ninja").hide();
-            $("#oldImg").hide();
-            $("#image-wrapper").append(this);
-            $(this).attr("id", "image");
-        }
-        //static method creates a DOMString containing a URL representing the object given in the parameter
-        image.src = _URL.createObjectURL(file);
-    }
-});
-$(document).ready(function(){
-  $("#size").click(function){
-    $("#divv").animate({
-      height: "400px",
-      width: "400px",
-    }, 1500);
-  },
-});
